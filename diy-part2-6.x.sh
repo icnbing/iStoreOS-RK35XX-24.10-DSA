7c26e97af246bd5ca0974f43fb93318bffe85336
xiaomeng9597 <519561910@qq.com>
2026-03-09 05:50:05 +0800

    add nsy_g68-plus

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -17,7 +17,7 @@ cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
 # ls package/base-files/files/etc/
 
 
-# 追加binder内核参数
+# 追加自定义内核配置项
 echo "CONFIG_PSI=y
 CONFIG_KPROBES=y" >> target/linux/rockchip/armv8/config-6.6
 
@@ -48,6 +48,17 @@ endef
 TARGET_DEVICES += bendian_bd-one" >> target/linux/rockchip/image/legacy.mk
 
 
+# 增加nsy_g68-plus
+echo -e "\\ndefine Device/nsy_g68-plus
+\$(call Device/Legacy/rk3568,\$(1))
+  DEVICE_VENDOR := NSY
+  DEVICE_MODEL := G68
+  DEVICE_DTS := rk3568/rk3568-nsy-g68-plus
+  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-hwmon-pwmfan kmod-thermal kmod-r8169
+endef
+TARGET_DEVICES += nsy_g68-plus" >> target/linux/rockchip/image/legacy.mk
+
+
 # 复制 02_network 网络配置文件到 target/linux/rockchip/armv8/base-files/etc/board.d/ 目录下
 cp -f $GITHUB_WORKSPACE/configfiles/02_network target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
@@ -67,3 +78,4 @@ ls target/linux/rockchip/files/drivers/net/dsa/
 # 复制dts设备树文件到指定目录下
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3588-orangepi-5-plus.dts target/linux/rockchip/dts/rk3588/rk3588-orangepi-5-plus.dts
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568-bendian-bd-one.dts target/linux/rockchip/dts/rk3568/rk3568-bendian-bd-one.dts
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568-nsy-g68-plus.dts target/linux/rockchip/dts/rk3568/rk3568-nsy-g68-plus.dts
